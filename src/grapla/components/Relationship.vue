c4b16d4abb77542e0b45f878b77aa820cc99da70
Lee Comstock <lee_comstock@hotmail.com>
2017-08-22 10:18:27 +0200

    bugfix: boxes not found in dom after window resize would throw error

--- src/grapla/components/Relationship.vue
+++ src/grapla/components/Relationship.vue
@@ -28,11 +28,16 @@ export default {
       var self = this;
       // wait until render is done
       Vue.nextTick(function () {
+        // get elements from dom
+        var fromElement = $('.grapla-box.' + self.data.from)[0];
+        var toElement = $('.grapla-box.' + self.data.to)[0];
+        var containerElement = $('#grapla')[0];
+        // end function if elements can't be found
+        if (!fromElement || !toElement || !containerElement) return;
         // get element attributes relative to document
-        var fromElement = $('.grapla-box.' + self.data.from)[0].getBoundingClientRect();
-        var toElement = $('.grapla-box.' + self.data.to)[0].getBoundingClientRect();
-        // get container attributes relative to document
-        var containerElement = $('#grapla')[0].getBoundingClientRect();
+        fromElement = fromElement.getBoundingClientRect();
+        toElement = toElement.getBoundingClientRect();
+        containerElement = containerElement.getBoundingClientRect();
         // get scroll values of container
         containerElement.scrollTop = $('#grapla')[0].scrollTop;
         containerElement.scrollLeft = $('#grapla')[0].scrollLeft;
