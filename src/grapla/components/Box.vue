1a7dcc9e8d7ed512ecc7423c9180a8f021c16604
Lee Comstock <lee_comstock@hotmail.com>
2017-08-24 16:27:39 +0200

    created dynamic component for dynamic importing of component type

--- src/grapla/components/Box.vue
+++ src/grapla/components/Box.vue
@@ -2,30 +2,24 @@
   <div :class="['grapla-box', data.id + '']">
     <p>{{data.id}}. {{data.text}}</p>
     <slot>
-      <component
+      <dynamic-component
         v-for="comp in data.children"
-        :is="comp.type"
         :data="comp"
         :key="comp.id">
-      </component>
+      </dynamic-component>
     </slot>
   </div>
 </template>
 
 <script>
+import dynamicComponent from './DynamicComponent.vue'
+
 export default {
   props: {
     data: Object
   },
-  created: function () {
-    // go through all child components
-    for(var comp of this.data.children) {
-      // check if child component has not been imported
-      if(!this.$options.components[comp.type]) {
-        // import correct component based on type
-        this.$options.components[comp.type] = require('./' + comp.type + '.vue');
-      }
-    }
+  components: {
+    dynamicComponent
   }
 }
 </script>
---
+++ src/grapla/components/DynamicComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="data.type" :data="data"></component>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  },
+  created: function () {
+    // import correct component based on type
+    this.$options.components[this.data.type] = require('./' + this.data.type + '.vue');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
